067bba0b0f3904e5a86e7a783e60274b66baccf9
Amardeep Ganguly <83149058+punyprogrammer@users.noreply.github.com>
2021-11-10 11:44:06 +0530

    Add files via upload

---
+++ day002_responsive_nav/app.js
@@ -0,0 +1,20 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  //toggleNav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+  //animate links
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${
+        index / 7 + 0.5
+      }s`;
+    }
+  });
+};
+navSlide();
